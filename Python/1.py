78172c2ccf476b660779870b4fab5320e11abe03
Kedar More <kedarmore5050@gmail.com>
2018-10-08 01:45:29 +0530

    Mid Sem Report

--- Python/1.py
+++ Python/1.py
@@ -24,36 +24,36 @@ for i in range(90):
         l += 1
         mesh[k, l] = point(a[(i+1) % 90, j % 90], (i+1), j, z)
         l += 1
-        
+
         mesh[k, l] = point(a[i % 90, (j+1) % 90], i, (j+1), x)  # third point
         l += 1
-        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, (j, y)
+        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, (j+1), y)
         l += 1
-        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, j, z)
+        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, (j+1), z)
 
         k+=1
 
         l = 1
 
-        mesh[k, l] = point(a[(i+1) % 90, (j+1) % 90], i, j, x)  # first point
+        mesh[k, l] = point(a[(i+1) % 90, (j+1) % 90], (i+1), (j+1), x)  # first point
         l += 1
-        mesh[k, l] = point(a[(i+1) % 90, j % 90], i, j, y)
+        mesh[k, l] = point(a[(i+1) % 90, (j+1) % 90], (i+1), (j+1), y)
         l += 1
-        mesh[k, l] = point(a[i % 90, j % 90], i, j, z)
-        l += 1
-
-        mesh[k, l] = point(a[(i+1) % 90, j % 90], i, j, x)  # second point
-        l += 1
-        mesh[k, l] = point(a[(i+1) % 90, j % 90], i, j, y)
-        l += 1
-        mesh[k, l] = point(a[(i+1) % 90, j % 90], i, j, z)
+        mesh[k, l] = point(a[(i+1) % 90, (j+1) % 90], (i+1), (j+1), z)
         l += 1
 
-        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, j, x)  # third point
+        mesh[k, l] = point(a[(i+1) % 90, j % 90], (i+1), j, x)  # second point
         l += 1
-        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, j, y)
+        mesh[k, l] = point(a[(i+1) % 90, j % 90], (i+1), j, y)
         l += 1
-        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, j, z)
+        mesh[k, l] = point(a[(i+1) % 90, j % 90], (i+1), j, z)
+        l += 1
+
+        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, (j+1), x)  # third point
+        l += 1
+        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, (j+1), y)
+        l += 1
+        mesh[k, l] = point(a[i % 90, (j+1) % 90], i, (j+1), z)
 
     pass
 pass
